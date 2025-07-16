a7fcd0f8d65a4406f3b1b5f1a4169ea0f9a22819
aswerty123 <44399805+aswerty123@users.noreply.github.com>
2025-07-16 03:40:03 +0000

    add telegram webhook

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask,render_template,request
 import joblib
 from groq import Groq
+import requests
 import os
 
 # os.environ['GROQ_API_KEY'] = os.getenv("GROQ_API_KEY")
+# TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 # for cloud ............
 
 app = Flask(__name__)
@@ -81,6 +83,28 @@ def dpsk_reply():
     # print(completion.choices[0].message.content)
 
     return(render_template("dpsk_reply.html", r=completion.choices[0].message.content))
+
+@app.route("/telegram",methods=["GET","POST"])
+def telegram():
+    # BASE_URL = f'https://api.telegram.org/bot{TOKEN}/'
+    domain_url = request.url_root  # Includes scheme (http/https) and domain
+    # domain_url = 'https://dsat-ft1-ipop.onrender.com'
+
+    # The following line is used to delete the existing webhook URL for the Telegram bot
+    delete_webhook_url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/deleteWebhook"
+    requests.post(delete_webhook_url, json={"url": domain_url, "drop_pending_updates": True})
+
+    # Set the webhook URL for the Telegram bot
+    set_webhook_url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/setWebhook?url={domain_url}/webhook"
+    webhook_response = requests.post(set_webhook_url, json={"url": domain_url, "drop_pending_updates": True})
+
+    if webhook_response.status_code == 200:
+        # set status message
+        status = "The telegram bot is running. Please check with the telegram bot. @your_bot"
+    else:
+        status = "Failed to start the telegram bot. Please check the logs."
+
+    return(render_template("telegram.html", status=status))
 
 if __name__ == "__main__":
     app.run()
